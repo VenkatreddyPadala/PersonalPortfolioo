0a962191e37d10e271b4ea0c91eca3f5bea6cdb6
VenkatreddyPadala <146629360+VenkatreddyPadala@users.noreply.github.com>
2023-10-02 23:17:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Header Toggle 
+let MenuBtn = document.getElementById('MenuBtn')
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+}) 
+
+// Typing Effect
+let typed = new Typed('.auto-input',{
+    strings:['Front-End Developer!','Free Lancer.','Web Designer.','Web Developer.'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+
+})
